950e58fad46e8ecd5382aff903c2f1bbe1b2c6d5
shamim-1995 <sashamim987@gmail.com>
2023-09-01 20:05:05 +0600

    added practice problem

---
+++ practice.js
@@ -0,0 +1,42 @@
+// What will be the output of the following JavaScript code?
+
+// function compare(a, b) {
+//  if (a == b) {
+//  return true;
+//  } else {
+//  return false;
+//  }
+// }
+// const result = compare(15, "15");
+// console.log(result);
+
+// What will be the output of the following JavaScript code? (Hey , Read Carefully)
+
+
+
+// function compare(a, b) {
+//  if (a.toString() === b) {
+//  return true;
+//  } else {
+//  return false;
+//  }
+// }
+// const result = compare(25, 25);
+// console.log(result);
+
+
+
+
+// What will be the output of the following JavaScript code? (Hint: Implicit type conversion)
+
+// console.log("123" + 123);
+
+
+
+// What will be the output of the following JavaScript code?
+
+// let a=”hi”
+// let b=”there”
+// console.log(a+b)
+
+
